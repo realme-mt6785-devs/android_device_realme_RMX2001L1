3b9af456fd1104f62a8aea087985dfe269246ad4
SamarV-121 <samar@samarv121.dev>
2023-11-11 12:10:25 +0530

    Initial commit
    Change-Id: I9593e31059429b1732b4726b40df4edd216b821c

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include device/realme/RM6785-common/BoardConfigCommon.mk
+
+# Partitions
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
