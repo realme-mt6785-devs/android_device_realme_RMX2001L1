bd2d8d75f659a9c49a96be1e45a38463dcec44e8
Debayan Kar <kardebayan3@gmail.com>
2023-11-25 20:23:32 +0530

    RMX2001L1: AOSP-ify

--- lineage_RMX2001L1.mk
+++ lineage_RMX2001L1.mk
@@ -5,7 +5,7 @@
 #
 $(call inherit-product, device/realme/RMX2001L1/full_RMX2001L1.mk)
 
-# Inherit common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_RMX2001L1
+PRODUCT_NAME := aosp_RMX2001L1
