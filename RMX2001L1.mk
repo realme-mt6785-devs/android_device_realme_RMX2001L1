86252877dbd2255d7331a36aa9b59a1927cf197c
SamarV-121 <samar@samarv121.dev>
2024-01-28 15:59:11 +0530

    RMX2001L1: LeafOS bringup
    Change-Id: Ib6c3db31c32802d07e211f18c8e02fc81c57fd6e

---
+++ RMX2001L1.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The LeafOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+$(call inherit-product, device/realme/RMX2001L1/full_RMX2001L1.mk)
+
+# Inherit common LeafOS stuff.
+$(call inherit-product, vendor/leaf/config/common_full_phone.mk)
+
+PRODUCT_NAME := RMX2001L1
