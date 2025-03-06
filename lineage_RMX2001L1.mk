e409983a594e7ed13295682c0e8077a5db343702
SamarV-121 <samar@samarv121.dev>
2025-03-06 09:55:13 +0530

    RMX2001L1: Override stock fingerprint
     * supposedly fixes the play certification
    
    Change-Id: Ib86ee2e3141d26b27d322d36fb19af4bfecc0db5

--- lineage_RMX2001L1.mk
+++ lineage_RMX2001L1.mk
@@ -9,3 +9,8 @@ $(call inherit-product, device/realme/RMX2001L1/full_RMX2001L1.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_RMX2001L1
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="sys_oplus_mssi_64_cn-user 11 RP1A.200720.011 1623809323039 release-keys" \
+    BuildFingerprint=realme/RMX2002/RMX2002L1:11/RP1A.200720.011/1651754371157:user/release-keys \
+    DeviceProduct=RMX2002
